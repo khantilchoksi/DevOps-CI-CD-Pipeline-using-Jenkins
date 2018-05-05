762ae29f9d4651888b72311e5ed114cdff5376bb
Khantil Choksi <khchoksi@ncsu.edu>
2018-05-05 00:25:49 -0400

    chaos cpu shell script

---
+++ monitoring/chaos_cpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script for CPU  Chaos
+
+cat << EOF > /tmp/infiniteburn.sh
+#!/bin/bash
+while true;
+    do openssl speed;
+done
+EOF
+
+#Will cause a ton of chaos! 
+for i in {1..32}
+do
+    nohup /bin/bash /tmp/infiniteburn.sh &
+done
